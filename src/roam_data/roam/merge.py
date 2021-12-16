f24a3e35ccd2947935237ebbb5b7c9777e2110c7
Romilly Cocking <romilly.cocking@gmail.com>
2021-12-16 13:01:40 +0000

    Tidying up

--- src/roam_data/roam/merge.py
+++ src/roam_data/roam/merge.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from roam_data.roam.graph import Block
+from markdown_builder.document import MarkdownDocument
 
 
 def merge_retros(rb: List[Block]):
@@ -10,3 +11,10 @@ def merge_retros(rb: List[Block]):
             block =retro.children[i]
             rr[i]+=block.children
     return rr
+
+
+def append_block_to_markdown(block: Block, doc: MarkdownDocument, level=0):
+    doc.append_bullet(block.string, level)
+    for child in block.children:
+        append_block_to_markdown(child, doc, level + 1)
+
---
+++ src/roam_data/roam/to_markdown.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from roam_data.roam.graph import Block
+from markdown_builder.document import MarkdownDocument
+
+
+def append_block_to_markdown(block: Block, doc: MarkdownDocument, level=0):
+    doc.append_bullet(block.string, level)
+    for child in block.children:
+        append_block_to_markdown(child, doc, level + 1)
+
+
+def retro_blocks_to_markdown(rr: List[Block]):
+    doc = MarkdownDocument()
+    headings = ['[WW]',['WDNW'],'WDD']
+    for (i, heading) in enumerate(headings):
+        doc.append_heading(heading)
+        for block in rr[i]:
+            append_block_to_markdown(block, doc)
+    with open('retro.md','w') as md:
+        md.write(doc.contents())
+    doc.close()
